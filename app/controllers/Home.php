7d3f939fdb37051ccb1762bb262a6ed649e8eb9c
abderrahmane <abderrahamane7@gmail.com>
2022-02-22 10:14:43 +0100

    removed main model

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,10 +1,6 @@
 <?php
 class Home extends Controller{
 
-    public function __construct(){
-        $this->model = $this->model("Main");
-    }
-
     public function index(){
         $this->view("home/index");
     }
